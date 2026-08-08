a5eab45ddc9d855a3997c63de30356c5211dc0e2
Harry S. Lion <harry.silverlion@icloud.com>
2026-08-08 21:10:57 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import tkinter
+
+def ende():
+    tkinter.messagebox.showinfo(title="Achtung!", message="Programm wird geschlossen!")
+    gui.destroy()
+
+gui = tkinter.Tk()
+
+abschluss = tkinter.Button(gui,text="Programm beenden", command=ende).pack()
+
+gui.mainloop()
